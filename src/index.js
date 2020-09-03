61de8b1b80e0a6539f30a5e7c42ee5f8963e478f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-03 19:49:05 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -97,7 +97,6 @@ console.log('actually axios', axios)
 //    * IN ANY CASE: log "done" to the console
 axios.get('https://dog.ceo/api/breed/mastiff/images/random/3')
   .then(stuff => {
-    debugger
     console.log('response body axios puts in "data" property', stuff.data)
     console.log('the array of dog images', stuff.data.message)
     const dogImageURLsArr = stuff.data.message
