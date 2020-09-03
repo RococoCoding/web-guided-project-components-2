3e76f4909f643ad81c3c407e97d3cfe332415f67
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-03 19:54:36 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -95,24 +95,28 @@ console.log('actually axios', axios)
 //    * ON SUCCESS: use the data to create dogCards and append them to the entry point
 //    * ON FAILURE: log the error to the console
 //    * IN ANY CASE: log "done" to the console
-axios.get('https://dog.ceo/api/breed/mastiff/images/randomMMMMMMMMMMMMMMMMMMMMMMMMMM/3')
-  .then(stuff => {
-    // THIS IS THE ONLY PLACE WHERE WE HAVE ACCESS TO THE IMAGE URLS FROM THE ENDPOINT
-    // THIS IS THE ONLY PLACE WHERE WE HAVE ACCESS TO THE IMAGE URLS FROM THE ENDPOINT
-    // THIS IS THE ONLY PLACE WHERE WE HAVE ACCESS TO THE IMAGE URLS FROM THE ENDPOINT
-    console.log('response body axios puts in "data" property', stuff.data)
-    console.log('the array of dog images', stuff.data.message)
-    const dogImageURLsArr = stuff.data.message
+// axios.get('https://dog.ceo/api/breed/mastiff/images/random/3')
+//   .then(stuff => {
+//     // THIS IS THE ONLY PLACE WHERE WE HAVE ACCESS TO THE IMAGE URLS FROM THE ENDPOINT
+//     // THIS IS THE ONLY PLACE WHERE WE HAVE ACCESS TO THE IMAGE URLS FROM THE ENDPOINT
+//     // THIS IS THE ONLY PLACE WHERE WE HAVE ACCESS TO THE IMAGE URLS FROM THE ENDPOINT
+//     console.log('response body axios puts in "data" property', stuff.data)
+//     console.log('the array of dog images', stuff.data.message)
+//     const dogImageURLsArr = stuff.data.message
 
-    dogImageURLsArr.forEach(URL => {
-      const dogCard = dogCardMaker({ imageURL: URL, breed: 'mastiff' })
-      entryPoint.appendChild(dogCard)
-    })
-    // function dogCardMaker expects this: { imageURL, breed }
-  })
-  .catch(err => {
-    console.log(err)
-    debugger
+//     dogImageURLsArr.forEach(URL => {
+//       const dogCard = dogCardMaker({ imageURL: URL, breed: 'mastiff' })
+//       entryPoint.appendChild(dogCard)
+//     })
+//     // function dogCardMaker expects this: { imageURL, breed }
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     debugger
+//   })
+fetch('https://dog.ceo/api/breed/mastiff/images/random/3')
+  .then(response => {
+    
   })
 
 // 👉 (OPTIONAL) TASK 6- Wrap the fetching operation inside a function `getDogs`
